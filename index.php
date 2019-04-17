11d052a91866f5165997d7ba55e7730168a6022e
Mozafari1 <m_rahmat@hotmail.no>
2019-04-17 11:10:30 +0200

    Completed Portfolio

--- index.php
+++ index.php
@@ -3,8 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <!-- Optional theme -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link rel="stylesheet" href="css/superslides.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+
+    <link rel="stylesheet" href="css/style.css">
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,21 +20,590 @@
     <title>Portfolio</title>
 </head>
 <body>
-    
+    <div class="loader">
+        <div class="inner">
+
+        </div>
+    </div>
 <div id="slides">
+
+
+    <div class="overlay"></div>
   <div class="slides-container">
     <img src="images/slide1.jpg" alt="">
     <img src="images/slide2.jpg" alt="">
     <img src="images/slide3.jpg" alt="">
-
   </div>
+     <div class="title">
+        <div class="heading">
+             <p class="main">rahmat mozafari</p>
+             <p class="sub typed"></p>
+        </div>
+     </div>
+  <nav class="slides-navigation">
+    <a href="#" class="forward"><i class="fas fa-chevron-left" style="font-size: 43px; color: white;"></i></a>
+    <a href="#" class="back"><i class="fas fa-chevron-right"style="font-size: 43px; color: white;"></i></a>
+  </nav>
 </div>
 
+<nav id="navigation" class="navbar navbar-expand-lg ">
+  <a class="navbar-brand" href="#">Rahmat Mozafari</a>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"><i class="fas fa-ellipsis-v"></i></span>
+  </button>
+ 
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#about">About <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#skills">Skills</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#stats">Stats</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#contact">Contact</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#portfolio">Portfolio</a>
+      </li>
+    </ul>
+  </div>
+
+</nav>
+
+<div id="about" class="section">
+<div class="container">
+     <div class="row">
+        <div class="col-md-5">
+            <img class="im" src="image/r.jpg" alt="">
+        </div>
+        <div class="col-md-7">
+                <h3 class= "spa">About</h3>
+                <hr>
+                <p class="aboutText">Hi, there! I'm Rahmat Mozafari from Oslo, Norway. I currently studying computer Engineering at University of South-Eastern Norway (USN). I have some experience creating software, mobile apps, and websites for both myself and for clients. I have experience prgramming in many different languages such as C++, C, Java, Kotlin, PHP, Python, SQL and Objective-C, however some of my favourite projects involve working in Swift for iOS.</p>
+                <p class="aboutText">Graduating in 2020 from Uinversity of South-Eastern Norway in Computer Science. </p>
+            </div>
+    </div>
+   </div>
+</div>
+
+<div id="skills" class="skillSec section">
+<div class="container">
+     <div class="row">
+        <div class="col-md-12 text-center">
+            <h2 class="spe">TECHNIAL SKILLS</h2>
+            <p class="spee">A representation of my proficiency in each skill</p>
+            <br>
+            <hr>
+        </div>
+    <div class="owl-carousel owl-theme">
+        <div class="skill">
+            <span class="chart" data-percent="67">
+                <span class="percent">67</span>
+                <canvas height="152" width="152"></canvas>
+
+            </span>
+                <h4>PHP</h4>
+                <p>Used in most of my web based projects</p>
+        </div>
+        <div class="skill">
+            <span class="chart" data-percent="63">
+                <span class="percent">63</span>
+                <canvas height="152" width="152"></canvas>
+            </span>
+                <h4>Java</h4>
+                <p>Used in most of my web based projects</p>
+        </div>
+        <div class="skill">
+            <span class="chart" data-percent="71">
+                <span class="percent">71</span>
+                <canvas height="152" width="152"></canvas>
+            </span>
+                <h4>C</h4>
+                <p>Used in most of my microcontroller based projects</p>
+        </div>
+        <div class="skill">
+            <span class="chart" data-percent="73">
+                <span class="percent">73</span>
+                <canvas height="152" width="152"></canvas>
+            </span>
+                <h4>C++</h4>
+                <p>Used in most of my application based projects</p>
+        </div>
+        <div class="skill">
+            <span class="chart" data-percent="58">
+                <span class="percent">58</span>
+                <canvas height="152" width="152"></canvas>
+            </span>
+                <h4>Kotlin</h4>
+                <p>Used in most of my mobile app based projects</p>
+        </div>
+        <div class="skill">
+            <span class="chart" data-percent="75">
+                <span class="percent">71</span>
+                <canvas height="152" width="152"></canvas>
+            </span>
+                <h4>SQL</h4>
+                <p>Used in most of my web based projects</p>
+        </div>
+        <div class="skill">
+            <span class="chart" data-percent="78">
+                <span class="percent">71</span>
+                <canvas height="152" width="152"></canvas>
+            </span>
+                <h4>Python</h4>
+                <p>Used in most of my API and other based projects</p>
+        </div>
+        <div class="skill">
+            <span class="chart" data-percent="64">
+                <span class="percent">64</span>
+                <canvas height="152" width="152"></canvas>
+            </span>
+                <h4>JS</h4>
+                <p>Used in most of my web based projects</p>
+        </div>
+    </div>
+
+    </div>
+    <hr>
+   </div>
+</div>
+
+<div id="stats" class="statsSec section">
+<div class="container">
+     <div class="row">
+        <div class="col-md-3 col-sm-6">
+            <div class="squareItem">
+                <div class="squareInnerContainer">
+                    <div class="squareIcon">
+                    <i class="far fa-clock"></i>
+
+                    </div>
+                    <div class="squareContent">
+                        <h2 class="counter">2</h2>
+                        <h3>Years experience</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="squareItem">
+                <div class="squareInnerContainer">
+                    <div class="squareIcon">
+                    <i class="fas fa-calendar-check"></i>
+                    </div>
+                    <div class="squareContent">
+                        <h2 class="counter">23</h2>
+                        <h3>Clients</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="squareItem">
+                <div class="squareInnerContainer">
+                    <div class="squareIcon">
+                    <i class="fab fa-android"></i>
+                    </div>
+                    <div class="squareContent">
+                        <h2 class="counter">4</h2>
+                        <h3>Andriod apps</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="squareItem">
+                <div class="squareInnerContainer">
+                    <div class="squareIcon">
+                    <i class="fas fa-cloud"></i>
+                    </div>
+                    <div class="squareContent">
+                        <h2 class="counter">5</h2>
+                        <h3>Websites Made</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+   </div>
+</div>
+<div id ="contact" class="contactSection section">
+    <div class="col-md-12 text-center">
+        <p class="subhaed">Like what you see?</p>
+        <h2>I'd love to hear from you!</h2>
+        <a href="mailTo:rahmat@mozafari.no" class="contactB">GET IN TOUCH!</a>
+    </div>
+</div>
+
+<div id="portfolio" class="section">
+
+		<div class="container">
+
+			<div class="row">
+
+				<div class="heading">
+					<h2>PORTFOLIO</h2>
+				</div>
+
+				<div class="filter">
+
+					<ul id="filters">
+
+						<li><a href="#" data-filter="*" class="current">All</a></li>
+						<li><a href="#" data-filter=".apps">Apps</a></li>
+						<li><a href="#" data-filter=".me">Me</a></li>
+						<li><a href="#" data-filter=".websites">Websites</a></li>
+
+					</ul>
+
+				</div>
+
+
+				<div class="itemsContainer">
+
+					<ul class="items">
+
+						<li onclick ="" class="apps col-xs-6 col-sm-4 col-md-3 col-lg-3">
+
+
+							<div class="item">
+
+								<img src="images/small/a1.jpg">
+
+
+								<div class="icons">
+									<a href="images/a1.jpg" title="View image" class="openButton" data-fancybox data-caption="Add new admin">
+										<i class="fa fa-search"></i>
+									</a>
+
+									<a href="" target="_blank" class="projectLink">
+										<i class="fa fa-link"></i>
+									</a>
+								</div>
+
+								<div class="imageOverlay"></div>
+
+							</div>
+
+
+						</li>
+
+						<li onclick ="" class="websites col-xs-6 col-sm-4 col-md-3 col-lg-3">
+
+
+							<div class="item">
+
+								<img src="images/small/w1.jpg">
+
+
+								<div class="icons">
+									<a href="images/w1.jpg" title="View image" class="openButton" data-fancybox data-caption="This is CSS file">
+										<i class="fa fa-search"></i>
+									</a>
+
+									<a href="https://lwr.no/" target="_blank" class="projectLink">
+										<i class="fa fa-link"></i>
+									</a>
+								</div>
+
+								<div class="imageOverlay"></div>
+
+							</div>
+
+
+						</li>
+
+						<li onclick ="" class="me col-xs-6 col-sm-4 col-md-3 col-lg-3">
+
+
+							<div class="item">
+
+								<img src="images/small/r1.jpg">
+
+
+								<div class="icons">
+									<a href="images/r1.jpg" title="View image" class="openButton" data-fancybox data-caption="This is me">
+										<i class="fa fa-search"></i>
+									</a>
+
+									<a href="https://mozafari.no/" target="_blank" class="projectLink">
+										<i class="fa fa-link"></i>
+									</a>
+								</div>
+
+								<div class="imageOverlay"></div>
+
+							</div>
+
+
+						</li>
+
+						<li onclick ="" class="websites col-xs-6 col-sm-4 col-md-3 col-lg-3">
+
+
+							<div class="item">
+
+								<img src="images/small/w2.jpg">
+
+
+								<div class="icons">
+									<a href="images/w2.jpg" title="View image" class="openButton" data-fancybox data-caption="This is contact file">
+										<i class="fa fa-search"></i>
+									</a>
+
+									<a href="https://github.com/Mozafari1" target="_blank" class="projectLink">
+										<i class="fa fa-link"></i>
+									</a>
+								</div>
+
+								<div class="imageOverlay"></div>
+
+							</div>
+
+
+						</li>
+
+						<li onclick ="" class="websites col-xs-6 col-sm-4 col-md-3 col-lg-3">
+
+
+							<div class="item">
+
+								<img src="images/small/w3.jpg">
+
+
+								<div class="icons">
+									<a href="images/w3.jpg" title="View image" class="openButton" data-fancybox data-caption="This is sub file">
+										<i class="fa fa-search"></i>
+									</a>
+
+									<a href="" target="_blank" class="projectLink">
+										<i class="fa fa-link"></i>
+									</a>
+								</div>
+
+								<div class="imageOverlay"></div>
+
+							</div>
+
+
+						</li>
+
+
+						<li onclick ="" class="apps col-xs-6 col-sm-4 col-md-3 col-lg-3">
+
+
+							<div class="item">
+
+								<img src="images/small/a2.jpg">
+
+
+								<div class="icons">
+									<a href="images/a2.jpg" title="View image" class="openButton" data-fancybox data-caption="This is blog page">
+										<i class="fa fa-search"></i>
+									</a>
+
+									<a href="" target="_blank" class="projectLink">
+										<i class="fa fa-link"></i>
+									</a>
+								</div>
+
+								<div class="imageOverlay"></div>
+
+							</div>
+
+
+                        </li>
+                    <li onclick =""  class="apps col-xs-6 col-sm-4 col-md-3 col-lg-3">
+
+
+                        <div class="item">
+
+                            <img src="images/small/a3.jpg">
+
+
+                            <div class="icons">
+                                <a href="images/a3.jpg" title="View image" class="openButton" data-fancybox data-caption="This is sign in page">
+                                    <i class="fa fa-search"></i>
+                                </a>
+
+                                <a href="" target="_blank" class="projectLink">
+                                    <i class="fa fa-link"></i>
+                                </a>
+                            </div>
+
+                            <div class="imageOverlay"></div>
+
+                        </div>
+
+
+                        </li>
+                <li onclick ="" class="me col-xs-6 col-sm-4 col-md-3 col-lg-3">
+
+
+                        <div class="item">
+
+                            <img src="images/small/r2.jpg">
+
+
+                            <div class="icons">
+                                <a href="images/r2.jpg" title="View image" class="openButton" data-fancybox data-caption="This is me">
+                                    <i class="fa fa-search"></i>
+                                </a>
+
+                                <a href="" target="_blank" class="projectLink">
+                                    <i class="fa fa-link"></i>
+                                </a>
+                            </div>
+
+                            <div class="imageOverlay"></div>
+
+                        </div>
+
+
+                        </li>
+             <li onclick ="" class="apps col-xs-6 col-sm-4 col-md-3 col-lg-3">
+
+
+                    <div class="item">
+
+                        <img src="images/small/a4.jpg">
+
+
+                        <div class="icons">
+                            <a href="images/a4.jpg" title="View image" class="openButton" data-fancybox data-caption="This is window panel">
+                                <i class="fa fa-search"></i>
+                            </a>
+
+                            <a href="" target="_blank" class="projectLink">
+                                <i class="fa fa-link"></i>
+                            </a>
+                        </div>
+
+                        <div class="imageOverlay"></div>
+
+                    </div>
+
+
+                    </li>
+ <li onclick ="" class="websites col-xs-6 col-sm-4 col-md-3 col-lg-3">
+
+
+<div class="item">
+
+    <img src="images/small/w4.jpg">
+
+
+    <div class="icons">
+        <a href="images/w4.jpg" title="View image" class="openButton" data-fancybox data-caption="This is index page">
+            <i class="fa fa-search"></i>
+        </a>
+
+        <a href="" target="_blank" class="projectLink">
+            <i class="fa fa-link"></i>
+        </a>
+    </div>
+
+    <div class="imageOverlay"></div>
+
+</div>
+
+
+</li>
+<li onclick ="" class="me col-xs-6 col-sm-4 col-md-3 col-lg-3">
+
+
+<div class="item">
+
+    <img src="images/small/r3.jpg">
+
+
+    <div class="icons">
+        <a href="images/r3.jpg" title="View image" class="openButton" data-fancybox data-caption="This is me">
+            <i class="fa fa-search"></i>
+        </a>
+
+        <a href="" target="_blank" class="projectLink">
+            <i class="fa fa-link"></i>
+        </a>
+    </div>
+
+    <div class="imageOverlay"></div>
+
+</div>
+
+
+</li>
+<li onclick ="" class="me col-xs-6 col-sm-4 col-md-3 col-lg-3">
+
+
+<div class="item">
+
+    <img src="images/small/r4.jpg">
+
+
+    <div class="icons">
+        <a href="images/r4.jpg" title="View image" class="openButton" data-fancybox data-caption="This is me">
+            <i class="fa fa-search"></i>
+        </a>
+
+        <a href="" target="_blank" class="projectLink">
+            <i class="fa fa-link"></i>
+        </a>
+    </div>
+
+    <div class="imageOverlay"></div>
+
+</div>
+
+
+</li>
+
+					</ul>
+
+				</div>
+
+
+			</div>
+
+		</div>
+
+	</div>
+
+ <div class="copyrightSection">
+     <div class="col-md-12 text-center">
+     <p class="lead text-center"><strong >Oslo, Norway | </strong> Created by | Rahmat Mozafari | &copy;<span id="year"></span> | All right reserved | Follow me on  
+          <a href="https://www.instagram.com/lp_with_rahmat/"><i class="fab fa-instagram" style="color:#F56040;"></i></a> 
+  <a href="https://www.youtube.com/channel/UCow-m8KxH7G0MiePPQeREBw"><i class="fab fa-youtube" style="color:red;"></i></a> 
+  <a href="https://www.facebook.com/R2hm2t.Davidsen"><i class="fab fa-facebook-f" ></i></a>
+<a href="https://www.linkedin.com/in/rahmat-mozafari-15844017b"><i class="fab fa-linkedin" style="color:cornflowerblue; "></i></a>
+  <a href="https://github.com/Mozafari1"><i class="fab fa-github" style="color:grey;"></i></a> 
+ 
+          </p>
+     </div>
+ </div>   
+
+ <script>
+  $('#year').text(new Date().getFullYear());
+</script>
+
 <script src="js/jquery.superslides.min.js"></script>
+<script src="js/jquery.easypiechart.min.js"></script>
+
+<script src="js/typed.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/countUp.js"></script>
+<script src="js/countUp-jquery.js"></script>
+<script src="https://unpkg.com/isotope-layout@3.0.6/dist/isotope.pkgd.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <script src="js/script.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
 
 </body>
 </html>
